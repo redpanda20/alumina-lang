5ca7b529992c6a15aaf0c64a67ada6bae38ee197
Alexandra S <ast194@uclive.ac.nz>
2023-09-01 16:49:56 +1200

    Autoformat

--- char_reader/src/lib.rs
+++ char_reader/src/lib.rs
@@ -1,12 +1,9 @@
-
-
 use std::io;
 use std::str;
 #[derive(Debug)]
 pub enum CharReaderError {
     IOError(std::io::Error),
     ReachedEOF,
-
 }
 impl From<std::io::Error> for CharReaderError {
     fn from(err: std::io::Error) -> Self {
@@ -15,36 +12,42 @@ impl From<std::io::Error> for CharReaderError {
 }
 impl From<str::Utf8Error> for CharReaderError {
     fn from(_: str::Utf8Error) -> Self {
-        CharReaderError::IOError(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid UTF8"))
+        CharReaderError::IOError(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Invalid UTF8",
+        ))
     }
 }
 
 pub struct CharReader<R: io::Read> {
-	inner: R,
-	buf: Box<[u8]>,
-    pos: usize, // Left side
-    filled: usize // Right side
+    inner: R,
+    buf: Box<[u8]>,
+    pos: usize,    // Left side
+    filled: usize, // Right side
 }
 
 const DEFAULT_BUF_SIZE: usize = 5_000;
 
-impl <R: io::Read> CharReader<R> {
-	pub fn new(inner: R) -> Self {
-		CharReader::with_capacity(DEFAULT_BUF_SIZE, inner)
-	}
+impl<R: io::Read> CharReader<R> {
+    pub fn new(inner: R) -> Self {
+        CharReader::with_capacity(DEFAULT_BUF_SIZE, inner)
+    }
 
-	pub fn with_capacity(capacity: usize, inner: R) -> Self {
+    pub fn with_capacity(capacity: usize, inner: R) -> Self {
         let buf = vec![0; capacity].into_boxed_slice();
-		Self { inner, buf, pos: 0, filled: 0 }
-	}
-
+        Self {
+            inner,
+            buf,
+            pos: 0,
+            filled: 0,
+        }
+    }
 
     pub fn next_char(&mut self) -> Result<char, CharReaderError> {
         // Enough bytes in the current buffer
         if self.pos + 4 >= self.filled {
-
             // Shift buffer to the start
-            self.buf.copy_within(self.pos .. self.filled, 0);
+            self.buf.copy_within(self.pos..self.filled, 0);
             self.filled -= self.pos;
 
             self.filled += self.inner.read(&mut self.buf)?;
@@ -53,17 +56,18 @@ impl <R: io::Read> CharReader<R> {
             }
             self.pos = 0;
         }
-        
+
         let char = str::from_utf8(&self.buf[self.pos..self.filled])?
-            .chars().next().expect("&str must be at least length 1");
+            .chars()
+            .next()
+            .expect("&str must be at least length 1");
 
         self.pos += char.len_utf8();
         Ok(char)
     }
-
 }
 
-impl <R: io::Read> Iterator for CharReader<R> {
+impl<R: io::Read> Iterator for CharReader<R> {
     type Item = char;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -87,8 +91,7 @@ mod tests {
 
     #[test]
     fn file_read() {
-        let file = File::open("../test.alo")
-            .expect("Unable to open file");
+        let file = File::open("../test.alo").expect("Unable to open file");
 
         let mut reader = CharReader::new(file);
         let mut string = String::new();
@@ -97,7 +100,7 @@ mod tests {
             match reader.next_char() {
                 Ok(ch) => string.push(ch),
                 Err(err) if matches!(err, CharReaderError::ReachedEOF) => break,
-                Err(err) => assert!(false, "Reader failed unexpectedly: {:?}", err)
+                Err(err) => assert!(false, "Reader failed unexpectedly: {:?}", err),
             }
         }
 
